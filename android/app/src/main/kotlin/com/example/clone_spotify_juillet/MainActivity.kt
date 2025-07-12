e70f5a82284d90996e0d83272ea47ed1077cd83c
ChebbiYahya <ChebbiYahya>
2025-07-12 12:55:53 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/clone_spotify_juillet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clone_spotify_juillet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:clone_spotify_juillet/utils/theme/custom_themes/test_showdialog.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/test_theme/test_appbar.dart';
+import 'screens/test_theme/test_bottomsheet.dart';
+import 'screens/test_theme/test_checkbox.dart';
+import 'screens/test_theme/test_chip.dart';
+import 'screens/test_theme/test_elevatebutton.dart';
+import 'screens/test_theme/test_outlinedbutton.dart';
+import 'screens/test_theme/test_text.dart';
+import 'screens/test_theme/test_textformfield.dart';
+import 'utils/theme/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+
+      home: TestShowDialog(),
+    );
+  }
+}
